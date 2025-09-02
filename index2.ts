1c93ca1d055a51e66deac143b27cb5dcb28ba2f3
Shash <ashishsinghlogic@gmail.com>
2025-09-02 23:45:43 +0530

    Update index2.ts

--- index2.ts
+++ index2.ts
@@ -1,3 +1,11 @@
 //How do you connect to an auto scaling group
 //they have libraries, sdk for these
 // @aws-sdk/client-auto-scaling
+
+import { AutoScaling, AutoScalingClient } from "@aws-sdk/client-auto-scaling";
+
+
+const client = new AutoScalingClient({region: "REGION", credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey : process.env.AWS_ACCESS_SECRET!
+}});
