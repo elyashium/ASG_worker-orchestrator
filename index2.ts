efdda75fcf1b1e4f131d406c3bef5a8268c06c04
Shash <ashishsinghlogic@gmail.com>
2025-09-02 23:50:35 +0530

    Update index2.ts

--- index2.ts
+++ index2.ts
@@ -2,10 +2,20 @@
 //they have libraries, sdk for these
 // @aws-sdk/client-auto-scaling
 
-import { AutoScaling, AutoScalingClient } from "@aws-sdk/client-auto-scaling";
+//we can also use http endpoints here instead of the SDK
+
+import {AutoScalingClient, SetDesiredCapacityCommand } from "@aws-sdk/client-auto-scaling";
 
 
 const client = new AutoScalingClient({region: "REGION", credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY!,
     secretAccessKey : process.env.AWS_ACCESS_SECRET!
 }});
+
+const command = new SetDesiredCapacityCommand({
+    AutoScalingGroupName: "name of the autoscling group in AWS",
+    DesiredCapacity: 3
+});
+
+const data = await client.send(command);
+console.log(data);
